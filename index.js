b789104a54dd8f3a5e9561d908fc26da5c717552
Swarna Jang Maden <limbujungmaden@gmail.com>
2023-11-26 17:13:35 +0545

    show books in grid

--- index.js
+++ index.js
@@ -15,15 +15,23 @@ function addBookToLibrary(title, author, pages, read) {
 addBookToLibrary("The Philosopher's Stone", "JK Rowling", 500, "read");
 addBookToLibrary("Game of Thrones", "George RR Martin", 500, "read");
 
-// console.log(myLibrary[0]);
-// document.addEventListener("DOMContentLoaded", () => {
-//   myLibrary.forEach((el) => {
-//     div = document.createElement("div");
-//     ul = document.createElement("ul");
-//     for (i = 0; i < 4; i++) {
-//       li = document.createElement("li");
-//       li.innerText = el;
-//       ul.appendChild("li");
-//     }
-//   });
-// });
+document.addEventListener("DOMContentLoaded", () => {
+  myLibrary.forEach((el) => {
+    card = document.createElement("div");
+    console.log(el);
+    para1 = document.createElement("p");
+    para1.innerText = el.title;
+    card.appendChild(para1);
+    para2 = document.createElement("p");
+    para2.innerText = `Written By: ${el.author}`;
+    card.appendChild(para2);
+    para3 = document.createElement("p");
+    para3.innerText = `Pages: ${el.pages}`;
+    card.appendChild(para3);
+    para4 = document.createElement("p");
+    para4.innerText = `Status: ${el.read}`;
+    card.appendChild(para4);
+    card.className = "card";
+    document.getElementById("card_container").appendChild(card);
+  });
+});
